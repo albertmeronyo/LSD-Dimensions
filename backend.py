36a59b27a0a431989c35f399c5af5236dbb2497c
Ingrid Arcas <ingrid.arcas@gmail.com>
2014-09-22 19:34:38 +0200

    first analytics figures be and fe

--- backend.py
+++ backend.py
@@ -9,6 +9,11 @@ import sys
 import traceback
 import os
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+from pymongo import Connection
+from bson.son import SON
+from pylab import *
 
 __VERSION = 0.1
 
@@ -46,7 +51,7 @@ def lsd_dimensions():
 
 @route('/dimensions/:id', method='GET')
 def get_dimension(id):
-    # Avoid this lazy load on demand!
+    # TODO: avoid this lazy load on demand
     local_json = None
     with open('data.json', 'r') as infile:
         local_json = json.load(infile)
@@ -74,6 +79,45 @@ def about():
 
 @route('/analytics', method='GET')
 def analytics():
+    # TODO: avoid this lazy load on demand
+
+    ### 1. Dim-freq distribution
+    dims = db.dimensions.aggregate([
+        {"$unwind" : "$dimensions"},
+        {"$group": {"_id": {"uri": "$dimensions.uri", "label": "$dimensions.label"}, 
+                    "dimensionsCount" : {"$sum" : 1}}},
+        {"$sort": SON([("dimensionsCount", -1)])}
+    ])
+
+    freqs = [dim["dimensionsCount"] for dim in dims["result"]]
+    dim_names = [dim["_id"]["uri"] for dim in dims["result"]]
+
+    x = np.array(range(0, len(dim_names)))
+    y = np.array(freqs)
+    my_xticks = dim_names
+    #plt.xticks(x, my_xticks, rotation=90)
+    plt.plot(x, y)
+    plt.grid(True)
+    plt.savefig('views/img/dim-freq.png', bbox_inches='tight')
+    plt.close()
+
+    ### 2. Endpoints using LSD dimensions
+    figure(1, figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
+
+    num_endpoints = db.dimensions.find({"endpoint" : {"$exists" : "1"}}).count()
+    with_dims = db.dimensions.find({"dimensions" : {"$exists" : "1"}}).count()
+    frac_with = (float(with_dims) / num_endpoints) * 100
+    frac_without = 100 - frac_with
+
+    labels = 'With dimensions', 'Without dimensions'
+    fracs = [frac_with, frac_without]
+    explode=(0, 0)
+
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
+    plt.savefig('views/img/endpoint-usage.png', bbox_inches='tight')
+    plt.close()
+    
     return template('analytics')
 
 # Static Routes
--- views/analytics.tpl
+++ views/analytics.tpl
@@ -4,7 +4,19 @@
 
 <h2>Analytics</h2>
 
-<p class="text-center">Coming soon</p>
+
+<div class="row">
+  <div class="col-md-6">
+    <h3>Dimension frequency distribution</h3>
+    <p class="text-justify">Distribution of distinct LSD dimensions according to the frequency in which they appear in LOD datasets.</p>
+    <img src="img/dim-freq.png" class="img-responsive img-rounded">
+  </div>
+  <div class="col-md-6">
+    <h3>Datasets using qb:DimensionProperty</h3>
+    <p class="text-justify">Proportion of LOD datasets using any qb:DimensionProperty.</p>
+    <img src="img/endpoint-usage.png" class="img-responsive img-rounded">
+  </div> 
+</div>
 
 </div>
 
